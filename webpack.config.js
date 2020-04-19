7758206c0b5aa05ad73adf246b0a7a363e22be0b
Bisvarup Mukherjee <bisvarup.m@flipkart.com>
2020-04-19 11:53:12 +0530

    set up the build system

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const babelOptions = {
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  plugins: ['@babel/plugin-proposal-class-properties'],
+};
+
+const dev = process.env.NODE_ENV === 'development';
+
+module.exports = {
+  mode: dev ? 'development' : 'production',
+  entry: path.join(__dirname, 'src'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: babelOptions,
+          },
+          {
+            loader: 'ts-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: babelOptions,
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+};
